9d3a0b2d90f37645e034754df8012a0b781b9ee0
Lexer-0 <45894853+Lexer-0@users.noreply.github.com>
2019-09-24 17:26:48 +0100

    adding a new Regiser Activity

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signUp.setOnClickListener(View.OnClickListener {
+            val intent=Intent(this,Register::class.java)
+            startActivity(intent)
 
-        
+        })
+
     }
 
 
